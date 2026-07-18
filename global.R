4a161aa0770d9e9a7afcefff443d9fc97ffc1cf1
Balaji Santhanam <balaji.srinivasan.santhanam@gmail.com>
2026-07-18 08:07:35 -0400

    Update global.R

--- global.R
+++ global.R
@@ -52,5 +52,5 @@ get_gcs_rds <- function(path) {
 }
 
 module_rbps <- get_gcs_rds("data/module_rbps_slim.RDS")
-#common_genes <- get_gcs_rds("data/common_genes_quant_0.1")
-common_genes <- get_gcs_rds("data/common_TCGA_genes_rds")
+common_genes <- get_gcs_rds("data/common_genes_quant_0.1")
+#common_genes <- get_gcs_rds("data/common_TCGA_genes_rds")
